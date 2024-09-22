13ee518ddea27392ee3123e2bfe831b718bfbd68
JiiXaa <anythingfx@gmail.com>
2024-09-22 18:32:38 +0100

    Add support for statically typed links (experimental).

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 
-import TestComponent from "@/components/TestComponent";
+import TestComponent from "@/components/test-component";
 
 import "./globals.css";
 
